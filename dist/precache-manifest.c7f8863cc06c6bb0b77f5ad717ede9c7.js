b9dacd409f9127dd1007b29f776227feffd881bc
Oousmane Oddo <oddomaximum@gmail.com>
2021-02-02 19:40:41 +0000

    run for production

---
+++ dist/precache-manifest.c7f8863cc06c6bb0b77f5ad717ede9c7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36079c1b718446c883f",
+    "url": "/css/chunk-vendors.8ad92b76.css"
+  },
+  {
+    "revision": "770c4026c088969e0ea97d29d71b7120",
+    "url": "/img/afro.jpeg"
+  },
+  {
+    "revision": "55927cee7672eb72738daf9aa02427e3",
+    "url": "/img/djig.jpeg"
+  },
+  {
+    "revision": "5db073b09ed0367e24468018d6c96745",
+    "url": "/img/laye.jpeg"
+  },
+  {
+    "revision": "5bed8dfe7a33eef48b157f51cc2ef739",
+    "url": "/img/loud.jpeg"
+  },
+  {
+    "revision": "1bf06ee5c989310d10f31615744805aa",
+    "url": "/img/smart.jpeg"
+  },
+  {
+    "revision": "2ad333f8d0e1b715b0910e91f03f8a99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc7abc8690cad433738",
+    "url": "/js/about.bc887976.js"
+  },
+  {
+    "revision": "c5de12c751cfb5d23148",
+    "url": "/js/app.d6ad69f9.js"
+  },
+  {
+    "revision": "e36079c1b718446c883f",
+    "url": "/js/chunk-vendors.4c948e24.js"
+  },
+  {
+    "revision": "d39d61073cc92cb95f2fb5a35273ad9b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
